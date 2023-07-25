e04c8a78f1b457e6e11ca4217bce55f5f6ef7e84
jihyenamgung <sophiang@naver.com>
2023-07-25 11:09:20 +0900

    Initial commit

---
+++ app/src/main/java/com/example/test/MainActivity.kt
@@ -0,0 +1,48 @@
+package com.example.test
+
+import android.annotation.SuppressLint
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.Menu
+import android.widget.Button
+import android.widget.DatePicker
+import com.example.test.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    val binding by lazy { ActivityMainBinding.inflate(layoutInflater)}
+
+
+    @SuppressLint("WrongViewCast")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+        binding.imageBtn.setOnClickListener{
+            val intent = Intent(this, SubActivity::class.java)
+            val dataPicker = findViewById<DatePicker>(R.id.datePicker)
+            val n = 2
+            intent.putExtra("year",dataPicker.year.toString())
+            intent.putExtra("month",(dataPicker.month+1).toString().padStart(n,'0'))
+            intent.putExtra("day",dataPicker.dayOfMonth.toString().padStart(n,'0'))
+            startActivity(intent)
+        }
+    }
+
+
+
+//    override fun onCreate(savedInstanceState: Bundle?) {
+//        super.onCreate(savedInstanceState)
+//        setContentView(R.layout.activity_main)
+//
+//    }
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        val menuInflater = menuInflater
+        menuInflater.inflate(R.menu.actionmenu, menu)
+        return true
+    }
+
+
+
+}
+
---
+++ app/src/main/java/com/example/test/SubActivity.kt
@@ -0,0 +1,60 @@
+package com.example.test
+
+import android.annotation.SuppressLint
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.Menu
+import android.widget.TextView
+import java.text.SimpleDateFormat
+import java.util.*
+
+class SubActivity : AppCompatActivity() {
+
+    @SuppressLint("SetTextI18n")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sub)
+
+        val tV1 = findViewById<TextView>(R.id.startDay)
+
+        var year = intent.getStringExtra("year")!!.toString()
+        var month = intent.getStringExtra("month")!!.toString()
+        var day = intent.getStringExtra("day")!!.toString()
+
+        tV1.text = "$year. $month. $day."
+
+        val df = SimpleDateFormat("yyyyMMdd")
+        val sd = df.parse("$year$month$day").time
+        val today = Calendar.getInstance().apply{
+            set(Calendar.HOUR_OF_DAY,0)
+            set(Calendar.MINUTE,0)
+            set(Calendar.SECOND,0)
+            set(Calendar.MILLISECOND,0)
+        }.time.time
+
+        val tV2 = findViewById<TextView>(R.id.dayN)
+
+        tV2.text = "${(today - sd) / (24 * 60 * 60 * 1000) + 1}일"
+
+
+
+
+
+//        val calendar = Calendar.getInstance()
+//        val year1 = calendar.get(Calendar.YEAR)
+//        val month1 = calendar.get(Calendar.MONTH)
+//        val dayOfMonth1 = calendar.get(Calendar.DAY_OF_MONTH)
+//        var today = Calendar.getInstance()
+//        var day1 = "$year-$month-$day 00:00:00"
+//        var day2 =
+
+
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        val menuInflater = menuInflater
+        menuInflater.inflate(R.menu.actionmenu, menu)
+        return true
+    }
+
+}
