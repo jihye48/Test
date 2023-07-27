468981b82088b6405f8c48410899c7e37e0622d0
jihye48 <sophiang@naver.com>
2023-07-27 15:32:08 +0900

    달력과 글쓰기 창에서 서브창으로 복귀

--- app/src/main/java/com/example/test/calActivity.kt
+++ app/src/main/java/com/example/test/calActivity.kt
@@ -1,11 +1,34 @@
 package com.example.test
 
+import android.annotation.SuppressLint
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 
 class calActivity : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_cal)
+
+        setSupportActionBar(findViewById(R.id.main_tool_bar))
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.returnsub, menu)
+        return true
+    }
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item!!.itemId){
+            R.id.action_calendar->{
+                val intent = Intent(this, SubActivity::class.java)
+                startActivity(intent)
+                return true
+            }
+        }
+
+        return super.onOptionsItemSelected(item)
     }
 }
