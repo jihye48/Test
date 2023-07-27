9a207f053007a633a67d26ca51240120a5f90b0a
suhwon0217 <say.ur.natie@gmail.com>
2023-07-27 13:50:09 +0900

    .

--- app/src/main/java/com/example/test/SubActivity.kt
+++ app/src/main/java/com/example/test/SubActivity.kt
@@ -194,38 +194,9 @@ class SubActivity : AppCompatActivity() {
         return true
     }
 
-//    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-//        when(item!!.itemId){
-//            R.id.action_calendar->{
-//                val intent = Intent(this, calActivity::class.java)
-//                startActivity(intent)
-//                return true
-//            }
-//            R.id.action_brush->{
-//                val intent = Intent(this, brushActivity::class.java)
-//                startActivity(intent)
-//                return true
-//            }
-//        }
-//
-//        return super.onOptionsItemSelected(item)
-//    }
 
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        var layoutParmas:WindowManager.LayoutParams=WindowManager.LayoutParams()
-        layoutParmas.flags=WindowManager.LayoutParams.FLAG_DIM_BEHIND
-        layoutParmas.dimAmount=0.8f
-        getWindow().setAttributes(layoutParmas)
-        when(item?.itemId){
-            R.id.action_calendar->{
-
-            }
-            R.id.action_brush->{
-
-            }
-        }
-        return super.onOptionsItemSelected(item)
     }
+
 
 
 
