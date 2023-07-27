b4fb74429a95ebc9f9993f98f1a786c2fedf7d72
jihye48 <sophiang@naver.com>
2023-07-27 12:42:12 +0900

    툴바 수정

--- app/src/main/java/com/example/test/SubActivity.kt
+++ app/src/main/java/com/example/test/SubActivity.kt
@@ -22,121 +22,133 @@ import java.util.*
 
 class SubActivity : AppCompatActivity() {
 
-    lateinit var plusButton:FloatingActionButton
+    lateinit var plusButton: FloatingActionButton
 
-    @SuppressLint("SetTextI18n")
+    @SuppressLint("SetTextI18n", "MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sub)
-        //시작날짜텍스트뷰
-        val tV1 = findViewById<TextView>(R.id.startDay)
-        //메인에서 imgBtn클릭할때 보낸 인텐트(만난 날짜) 받기
+
+        setSupportActionBar(findViewById(R.id.main_tool_bar))
+
+        val tV1 = findViewById<TextView>(R.id.startDay) //만난 날짜(sd) 텍스트 뷰
+        //메인에서 imageBtn클릭할때 보낸 인텐트(만난 날짜) 받기
         var year = intent.getStringExtra("year")!!.toString()
         var month = intent.getStringExtra("month")!!.toString()
         var day = intent.getStringExtra("day")!!.toString()
-        //startDay텍스트 지정
-        tV1.text = "$year. $month. $day."
-        //String 날짜 형식 정의
-        val df = SimpleDateFormat("yyyyMMdd")
-        //시작날짜 형변환
+
+        tV1.text = "$year. $month. $day." //sd 출력
+
+        val df = SimpleDateFormat("yyyyMMdd") //String 날짜 형식 정의
+        //시작 날짜 형변환
         val sd = df.parse("$year$month$day").time
-        val today = Calendar.getInstance().apply{
-            set(Calendar.HOUR_OF_DAY,0)
-            set(Calendar.MINUTE,0)
-            set(Calendar.SECOND,0)
-            set(Calendar.MILLISECOND,0)
+        val today = Calendar.getInstance().apply {
+            set(Calendar.HOUR_OF_DAY, 0)
+            set(Calendar.MINUTE, 0)
+            set(Calendar.SECOND, 0)
+            set(Calendar.MILLISECOND, 0)
         }.time.time
 
-        //함께한지 n일 텍스트뷰
-        val tV2 = findViewById<TextView>(R.id.dayN)
-        //현재날짜-시작날짜
-        tV2.text = "${(today - sd) / (24 * 60 * 60 * 1000) + 1}일"
+        val tV2 = findViewById<TextView>(R.id.dayN) //함께한 지 n일 텍스트 뷰
+
+        tV2.text = "${(today - sd) / (24 * 60 * 60 * 1000) + 1}일" //sd일부터 며칠 째인지 출력
 
         //현재 날짜 정보 받기
         val calendar = Calendar.getInstance()
         val year1 = calendar.get(Calendar.YEAR)
         val month1 = calendar.get(Calendar.MONTH)
         val dayOfMonth1 = calendar.get(Calendar.DAY_OF_MONTH)
-        val date:Date=Date()
-        calendar.time=date
-
+        val date: Date = Date()
+        calendar.time = date
 
         // (res/values/array.xml)에 만들어둔 dday_list가져오기
-        var ddayList=resources.getStringArray(R.array.dday_list)
-        val myAdapter=ArrayAdapter(this, android.R.layout.simple_spinner_dropdown_item,ddayList)
-        var spinner1:Spinner=findViewById(R.id.spinner1)
-        var dday1:TextView=findViewById(R.id.dday1)
-        spinner1.adapter=myAdapter
+        var ddayList = resources.getStringArray(R.array.dday_list)
+        val myAdapter = ArrayAdapter(this, android.R.layout.simple_spinner_dropdown_item, ddayList)
+        var spinner1: Spinner = findViewById(R.id.spinner1)
+        var dday1: TextView = findViewById(R.id.dday1)
+        spinner1.adapter = myAdapter
         //spinner1 선택 리스너
-        spinner1.onItemSelectedListener=object:AdapterView.OnItemSelectedListener{
+        spinner1.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
             //선택하지 않았을때
             override fun onNothingSelected(p0: AdapterView<*>?) {
-                dday1.text="선택해주세요"
+                dday1.text = "선택해주세요"
             }
-            override fun onItemSelected(parent: AdapterView<*>, view: View, position: Int, id: Long) {
-                var year=year.toInt()
-                var month=month.toInt()
-                var day=day.toInt()
-                when(position){
+
+            override fun onItemSelected(
+                parent: AdapterView<*>,
+                view: View,
+                position: Int,
+                id: Long
+            ) {
+                var year = year.toInt()
+                var month = month.toInt()
+                var day = day.toInt()
+                when (position) {
                     //오늘  (month가 1 작게 나와서 1 더함)
                     0 -> {
-                        dday1.text="$year1.${month1+1}. $dayOfMonth1"
+                        dday1.text = "$year1.${month1 + 1}. $dayOfMonth1"
                     }
                     //처음만난날
                     1 -> {
-                        dday1.text="$year. $month. $day."
+                        dday1.text = "$year. $month. $day."
                     }
                     //10일
-                    2->{
-                        day+=10
-                        when(month){
-                            1,3,5,7,8,10,12 -> {
-                                if(day>31){
-                                    month+=1
-                                    day-=31
+                    2 -> {
+                        day += 10
+                        when (month) {
+                            1, 3, 5, 7, 8, 10, 12 -> {
+                                if (day > 31) {
+                                    month += 1
+                                    day -= 31
                                 }
                             }
-                            2,4,6,9,11 -> {
-                                if(day>30){
-                                    month+=1
-                                    day-=30
+
+                            2, 4, 6, 9, 11 -> {
+                                if (day > 30) {
+                                    month += 1
+                                    day -= 30
                                 }
                             }
-                            2->{
-                                if(day>28){
-                                    month+=1
-                                    day-=28
+
+                            2 -> {
+                                if (day > 28) {
+                                    month += 1
+                                    day -= 28
                                 }
                             }
                         }
-                        dday1.text="$year. $month. $day"
+                        dday1.text = "$year. $month. $day"
                     }
                     //50일
-                    3->{
-                        dday1.text="$year. $month. $day"
+                    3 -> {
+                        dday1.text = "$year. $month. $day"
                     }
                     //100일
-                    4->{
-                        dday1.text="$year. $month. $day"
+                    4 -> {
+                        dday1.text = "$year. $month. $day"
                     }
                     //1년
-                    5->{
-                        year+=1
-                        dday1.text="$year. $month. $day"
-                        if((year1==year)&&(month1==month-1)&&(dayOfMonth1==day))
-                            Toast.makeText(this@SubActivity, "1주년을 축하합니다!",Toast.LENGTH_SHORT).show()
+                    5 -> {
+                        year += 1
+                        dday1.text = "$year. $month. $day"
+                        if ((year1 == year) && (month1 == month - 1) && (dayOfMonth1 == day))
+                            Toast.makeText(this@SubActivity, "1주년을 축하합니다!", Toast.LENGTH_SHORT)
+                                .show()
                     }
                 }
             }
         }
-        plusButton=findViewById(R.id.plusButton)
-        plusButton.setOnClickListener{
-            Toast.makeText(this@SubActivity, "fab버튼 클릭",Toast.LENGTH_SHORT).show()
+        plusButton = findViewById(R.id.plusButton)
+        plusButton.setOnClickListener {
+            Toast.makeText(this@SubActivity, "fab버튼 클릭", Toast.LENGTH_SHORT).show()
 
         }
 
 
     }
+
+
+
     /*현재 날짜로부터 더하기
     fun afterDate(date: String, day: Int, pattern: String = "yyyy. MM. dd"): String {
         val format = SimpleDateFormat(pattern, Locale.getDefault())
@@ -150,10 +162,26 @@ class SubActivity : AppCompatActivity() {
 
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        val menuInflater = menuInflater
         menuInflater.inflate(R.menu.actionmenu, menu)
         return true
     }
+
+//    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+//        when(item!!.itemId){
+//            R.id.action_calendar->{
+//                val intent = Intent(this, calActivity::class.java)
+//                startActivity(intent)
+//                return true
+//            }
+//            R.id.action_brush->{
+//                val intent = Intent(this, brushActivity::class.java)
+//                startActivity(intent)
+//                return true
+//            }
+//        }
+//
+//        return super.onOptionsItemSelected(item)
+//    }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         var layoutParmas:WindowManager.LayoutParams=WindowManager.LayoutParams()
